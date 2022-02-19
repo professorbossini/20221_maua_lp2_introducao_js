449e7db6892d5a951c44fa15d1b8a06a22f0d9fd
Rodrigo Bossini <professorbossini@gmail.com>
2022-02-19 12:28:42 -0300

    retomar da secao 1.2, falando de tipos

---
+++ index.js
@@ -0,0 +1,30 @@
+var idade = 18
+console.log ("Oi, " + nome)
+if (idade >= 18){
+    let nome = "João"
+    console.log ("Parabéns, " + nome)
+    console.log("Você pode dirigir")
+}
+console.log ("Até mais " + nome)
+
+
+// var linguagem = "Javascript"
+// console.log ("Aprendendo " + linguagem)
+// //variável pode ser redeclarada
+// var linguagem = "Java"
+// console.log ("Aprendendo " + linguagem)
+// const nome = 'José " "'
+// nome = "abc"
+// const endereco = `Rua K, 12`
+// const sexo = "F ' ' "
+// const idade = 27
+
+// let a = 2
+// a = 3
+// let nomeCompleto = "Joao Silva"
+// a = "abc"
+
+
+// var c = 2 + 3
+// c = 5
+// var d = "abcd"
